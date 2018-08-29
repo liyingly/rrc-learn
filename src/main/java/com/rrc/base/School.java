596536cdce25a57b0562f9043866a89783079503
李盈 <2292668286@qq.com>
2018-08-29 12:09:04 +0800

    提交school和person类

---
+++ src/main/java/com/rrc/base/School.java
@@ -0,0 +1,34 @@
+package com.rrc.base;
+
+/**
+ * Created by LiYing on 2018/8/29.
+ */
+public class School {
+    private String name;
+    private String address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
