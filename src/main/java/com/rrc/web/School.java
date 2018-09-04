5a7a6ab9a872efcbd846d42ac0ac979d8fd9b128
李盈 <2292668286@qq.com>
2018-09-04 11:42:58 +0800

    B

--- src/main/java/com/rrc/web/School.java
+++ src/main/java/com/rrc/web/School.java
@@ -1,13 +1,40 @@
 package com.rrc.web;
 
+import java.util.List;
+
 /**
  * Created by LiYing on 2018/9/4.
  */
 public class School {
     private String name;
+    private int age;
+    private List<String> teacher;
     private Integer size;
     private Integer personNum;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(List<String> teacher) {
+        this.teacher = teacher;
+    }
     public String getName() {
         return name;
     }
