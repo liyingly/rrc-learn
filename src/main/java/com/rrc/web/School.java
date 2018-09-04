955031efd527c2d4d1c1437fa67c0b40efe66df8
李盈 <2292668286@qq.com>
2018-09-04 11:03:23 +0800

    A-School-2

--- src/main/java/com/rrc/web/School.java
+++ src/main/java/com/rrc/web/School.java
@@ -5,5 +5,30 @@ package com.rrc.web;
  */
 public class School {
     private String name;
+    private Integer size;
+    private Integer personNum;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getPersonNum() {
+        return personNum;
+    }
+
+    public void setPersonNum(Integer personNum) {
+        this.personNum = personNum;
+    }
 }
