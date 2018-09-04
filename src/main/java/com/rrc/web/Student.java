d893e1412948d2bff9b9d485f1d7fa58cbf98aaf
李盈 <2292668286@qq.com>
2018-09-04 10:51:03 +0800

    B

---
+++ src/main/java/com/rrc/web/Student.java
@@ -0,0 +1,16 @@
+package com.rrc.web;
+
+/**
+ * Created by LiYing on 2018/9/4.
+ */
+public class Student {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
